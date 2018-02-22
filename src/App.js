c3ad8bfac4e37ca38b4e3243c9e867f63277e742
Zhenzhen You <swimreally@MacBook-Air.local>
2018-02-22 14:44:03 -0800

    Learning React with Create-React-App part1-2

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import HelloWorld from './HelloWorld';
+
+const App = () => {
+  return (
+    <div className="App">
+    <HelloWorld name="Jim"/>
+    <HelloWorld name="Sally"/>
+    </div>
+  );
+};
+
+export default App;
